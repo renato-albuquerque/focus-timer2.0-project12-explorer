62239b63aedc0f942ef4dde67dee96e0418a4b80
renato-albuquerque <renatomalb83@gmail.com>
2023-01-27 10:04:19 -0300

    added js consts

---
+++ index.js
@@ -0,0 +1,21 @@
+const minutesDisplay = document.querySelector(".play")
+const secondsDisplay = document.querySelector(".seconds")
+const buttonPlay = document.querySelector(".play")
+const buttonPause = document.querySelector(".pause")
+const buttonStop = document.querySelector(".stop")
+const buttonPlus = document.querySelector(".plus")
+const buttonMinus = document.querySelector(".minus")
+const buttonForest = document.querySelector(".forest")
+const buttonRain = document.querySelector(".rain")
+const buttonCoffeeShop = document.querySelector(".coffee-shop")
+const buttonFirePlace = document.querySelector(".fire-place")
+
+const buttonPressAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
+const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
+
+const audioForest = new Audio("audios/audio-forest.wav")
+const audioRain = new Audio ("audios/audio-rain.wav")
+const audioCofeeShop = new Audio("audios/coffeeshop.wav")
+const audioFirePlace = new Audio("audios/fireplace.wav")
+
+
