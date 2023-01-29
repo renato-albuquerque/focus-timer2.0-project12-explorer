169fd5274da58c91ad49de230eabb0931382ea22
renato-albuquerque <renatomalb83@gmail.com>
2023-01-29 01:25:29 -0300

    added events

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//CONSTS
+
 const minutesDisplay = document.querySelector(".play")
 const secondsDisplay = document.querySelector(".seconds")
 const buttonPlay = document.querySelector(".play")
@@ -18,4 +20,87 @@ const audioRain = new Audio ("audios/audio-rain.wav")
 const audioCoffeeShop = new Audio("audios/audio-coffeeshop.wav")
 const audioFirePlace = new Audio("audios/audio-fireplace.wav")
 
+// FUNCTIONS
 
+function resetControls() {
+    buttonPlay.classList.remove("hide")
+    buttonPause.classList.add("hide")
+}
+
+function pressButton() {
+    buttonPressAudio.play()
+  }
+  
+function timeEnd() {
+    kitchenTimer.play()
+  }
+
+function soundForest() {
+    audioForest.play()
+}  
+
+function soundRain() {
+    audioRain.play()
+}
+
+function soundCoffeeShop() {
+    audioCoffeeShop.play()
+}
+
+function soundFirePlace() {
+    audioFirePlace.play()
+}
+
+// EVENTS
+
+buttonPlay.addEventListener("click", function() {
+    buttonPlay.classList.add("hide")
+    buttonPause.classList.remove("hide")
+
+    pressButton()
+})
+
+buttonPause.addEventListener("click", function() {
+    buttonPause.classList.add("hide")
+    buttonPlay.classList.remove("hide")
+
+    pressButton()
+})
+
+buttonStop.addEventListener("click", function() {
+    resetControls()
+    pressButton()
+})
+
+buttonPlus.addEventListener("click", function() {
+    minutesDisplay + 5
+
+    pressButton()
+})
+
+buttonMinus.addEventListener("click", function() {
+
+    pressButton()
+
+})
+
+buttonForest.addEventListener("click", function() {
+    
+    soundForest()
+})
+
+buttonRain.addEventListener("click", function() {
+    
+    soundRain()
+})
+
+buttonCoffeeShop.addEventListener("click", function() {
+    
+    soundCoffeeShop()
+    
+})
+
+buttonFirePlace.addEventListener("click", function() {
+    
+    soundFirePlace()
+})
